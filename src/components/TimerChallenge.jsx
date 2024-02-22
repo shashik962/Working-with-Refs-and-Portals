044feab09ca6e33a240803db2c9e3017e4d70ccc
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-23 00:12:52 +0530

    Adding Challenges to the Demo Project

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,20 @@
+
+
+export default function TimerChallenge({title, targetTime}) {
+      return (
+            <section className="challenge">
+                  <h2>{title}</h2>
+                  <p className="challenge-time">
+                        {targetTime} second{targetTime > 1 ? 's': ''}
+                  </p>
+                  <p>
+                        <button>
+                              Start Challenge
+                        </button>
+                  </p>
+                  <p>
+                        Time is running... / Time inactive
+                  </p>
+            </section>
+      )
+}
