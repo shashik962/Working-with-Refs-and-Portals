4a16a6e62d333b6164911ceaaf8d2f767430c1ff
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-27 12:27:43 +0530

    Sharing State Across Components or Enhancing the Demo App 'Result Modal'

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
       <Player />
       <div id="challenges">
          <TimerChallenge title="Easy" targetTime={1} />
-         <TimerChallenge title="Not easy" targetTime={1} />
-         <TimerChallenge title="Getting tough" targetTime={1} />
-         <TimerChallenge title="Pros only" targetTime={1} />
+         <TimerChallenge title="Not easy" targetTime={5} />
+         <TimerChallenge title="Getting tough" targetTime={10} />
+         <TimerChallenge title="Pros only" targetTime={15} />
       </div>
     </>
   );
--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,12 @@
 
-import { forwardRef, useImperativeHandle } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
-const ResultModal  = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal  = forwardRef(function ResultModal({result, targetTime, remainingTime, onReset}, ref) {
    const dialog = useRef();
+
+   const userLost = remainingTime <= 0;
+   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+   const score = Math.round((1- remainingTime / (targetTime * 1000)) * 100);
 
    useImperativeHandle(ref, () => {
       return {
@@ -14,10 +18,11 @@ const ResultModal  = forwardRef(function ResultModal({result, targetTime}, ref) 
 
    return (
          <dialog ref={dialog} className="result-modal">
-            <h2>Your {result} </h2>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: { score }</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{ formattedRemainingTime } seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                   <button>Close</button>
             </form>
          </dialog>
