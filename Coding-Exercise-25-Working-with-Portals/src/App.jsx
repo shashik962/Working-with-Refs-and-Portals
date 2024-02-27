edb915683f4008c84d9bb30ab9dda4cf97926dff
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-27 12:56:22 +0530

    Coding Exercise 20: Answer

--- Coding-Exercise-25-Working-with-Portals/src/App.jsx
+++ Coding-Exercise-25-Working-with-Portals/src/App.jsx
@@ -1,17 +1,21 @@
+import React from 'react';
+ 
 import Toast from './Toast';
-
+ 
 function App() {
+  const [toastVisible, setToastVisible] = React.useState(false);
+ 
   function handleEnrol() {
-    // Todo: Show toast
-
+    setToastVisible(true);
+ 
     setTimeout(() => {
-      // Todo: hide toast
+      setToastVisible(false);
     }, 3000);
   }
-
+ 
   return (
     <div id="app">
-      {/* Todo: Render <Toast /> component (conditionally) here */}
+      {toastVisible && <Toast message="Enrolled successfully!" />}
       <article>
         <h2>React Course</h2>
         <p>
@@ -22,5 +26,37 @@ function App() {
     </div>
   );
 }
-
+ 
 export default App;
+
+
+
+
+
+
+// import Toast from './Toast';
+
+// function App() {
+//   function handleEnrol() {
+//     // Todo: Show toast
+
+//     setTimeout(() => {
+//       // Todo: hide toast
+//     }, 3000);
+//   }
+
+//   return (
+//     <div id="app">
+//       {/* Todo: Render <Toast /> component (conditionally) here */}
+//       <article>
+//         <h2>React Course</h2>
+//         <p>
+//           A course that teaches you React from the ground up and in great depth!
+//         </p>
+//         <button onClick={handleEnrol}>Enrol</button>
+//       </article>
+//     </div>
+//   );
+// }
+
+// export default App;
--- Coding-Exercise-25-Working-with-Portals/src/Toast.jsx
+++ Coding-Exercise-25-Working-with-Portals/src/Toast.jsx
@@ -1,7 +1,19 @@
+
+import ReactDOM from 'react-dom';
+
 export default function Toast({ message }) {
-  return (
+  return ReactDOM.createPortal(
     <aside className="toast" data-testid="toast">
       <p>{message}</p>
-    </aside>
+    </aside>,
+    document.querySelector('body')
   );
 }
+
+// export default function Toast({ message }) {
+//   return (
+//     <aside className="toast" data-testid="toast">
+//       <p>{message}</p>
+//     </aside>
+//   );
+// }
