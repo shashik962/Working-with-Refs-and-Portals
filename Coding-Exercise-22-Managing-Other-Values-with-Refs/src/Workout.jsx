872f869fa549c80424b46ddea76db83d83f2e35a
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-27 09:46:06 +0530

    Coding Exercise 22:Answer

--- Coding-Exercise-22-Managing-Other-Values-with-Refs/src/Workout.jsx
+++ Coding-Exercise-22-Managing-Other-Values-with-Refs/src/Workout.jsx
@@ -1,23 +1,26 @@
+import React from 'react';
+ 
 export default function Workout({ title, description, time, onComplete }) {
-      function handleStartWorkout() {
-        // Todo: Start timer
-      }
-    
-      function handleStopWorkout() {
-        // Todo: Stop timer
-        onComplete();
-      }
-    
-      return (
-        <article className="workout">
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <p>{time}</p>
-          <p>
-            <button onClick={handleStartWorkout}>Start</button>
-            <button onClick={handleStopWorkout}>Stop</button>
-          </p>
-        </article>
-      );
-    }
-    
+  const timer = React.useRef();
+ 
+  function handleStartWorkout() {
+    timer.current = setTimeout(handleStopWorkout, time);
+  }
+ 
+  function handleStopWorkout() {
+    clearTimeout(timer.current);
+    onComplete();
+  }
+ 
+  return (
+    <article className="workout">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p>{time}</p>
+      <p>
+        <button onClick={handleStartWorkout}>Start</button>
+        <button onClick={handleStopWorkout}>Stop</button>
+      </p>
+    </article>
+  );
+}
